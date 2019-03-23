a043968e101a04ed350a3dbfb649101345f4a3ed
Ademílson F. Tonato <ademilsonft@outlook.com>
2019-03-23 23:12:41 +0000

    (fix) fixing gists url and removing temp files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 echo
-echo " # download default gitconfig / bash_profile "
-curl -o .gitconfig https://gist.githubusercontent.com/ftonato/d8f16af239643893bca44f4f876133c3/raw/4cb87d6e2d365ef96727e8c958b9e0090e646460/.gitconfig
-curl -o .bash_profile https://gist.githubusercontent.com/ftonato/f8e720d5a8c8dd6792824f956afd7840/raw/a543584eeb5bb4275c5b736f8bbaf32c79bee08a/.bash_profile
+echo " # download default gitconfig / bash_profile"
+curl -o .gitconfig https://gist.githubusercontent.com/ftonato/d8f16af239643893bca44f4f876133c3/raw/.gitconfig
+curl -o .bash_profile https://gist.githubusercontent.com/ftonato/f8e720d5a8c8dd6792824f956afd7840/raw/.bash_profile
 
 echo
-echo " # copy default gitconfig / bash_profile "
+echo " # copy default gitconfig / bash_profile"
 cp .gitconfig ~/.gitconfig
 cp .bash_profile ~/.bash_profile
+
+echo
+echo " # removing unnecessary temporary files"
+rm .gitconfig .bash_profile
 
 source ~/.bash_profile
