45d7265b8c5989d877962d537190167c71e361a7
Ademílson F. Tonato <5417662+ftonato@users.noreply.github.com>
2021-03-24 14:20:18 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 echo
 echo " # download default gitconfig / bash_profile"
-curl -o .gitconfig https://gist.githubusercontent.com/ftonato/d8f16af239643893bca44f4f876133c3/raw/.gitconfig
-curl -o .bash_profile https://gist.githubusercontent.com/ftonato/f8e720d5a8c8dd6792824f956afd7840/raw/.bash_profile
+curl -o .gitconfig https://gistcdn.githack.com/ftonato/d8f16af239643893bca44f4f876133c3/raw/.gitconfig
+curl -o .bash_profile https://gistcdn.githack.com/ftonato/f8e720d5a8c8dd6792824f956afd7840/raw/.bash_profile
 
 echo
 echo " # copy default gitconfig / bash_profile"
